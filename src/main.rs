e76b77387e8a0f9be9c581430a304c083fb11ff2
Ryan Swanson <ryan.s.swanson@gmail.com>
2023-01-05 22:02:31 -0500

    initial implementation work

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,45 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        error(err);
+        process::exit(1);
+    });
 
-    // println!("In file: {}", path);
-    let contents = fs::read_to_string(path)
+    let runner = run(config).unwrap_or_else(|err| {
+        error(err);
+        process::exit(1);
+    });
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config { 
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough args");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        
+        Ok( Config { query, file_path } )
+    }
+}
+
+fn run(config: Config) -> Result<bool, &'static str> {
+    let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file but could not");
 
-    // println!("With text: {}",contents);
+    println!("{contents}");
+    Ok(true)
+}
 
+fn error(message: &str) {
+    println!("Problem parsing arguments: {message}");
 }
