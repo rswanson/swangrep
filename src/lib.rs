44aab08582e01e345dc885c247306489498379b6
Ryan Swanson <ryan@nomad.xyz>
2023-01-06 11:44:29 -0500

    finishing implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,87 @@
-use std::fs;
 use std::error::Error;
-
+use std::fs;
+use std::env;
 
 pub struct Config {
     query: String,
     file_path: String,
+    ignore_case: bool,
 }
 
-impl Config { 
+impl Config {
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough args");
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
-        
-        Ok( Config { query, file_path } )
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config { query, file_path, ignore_case })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
-    println!("{contents}");
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+    
+    for line in results {
+        println!("{line}");
+    }
     Ok(())
 }
 
 pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
 }
+
+pub fn search_case_insensitive<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+    }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
-    fn one_result() {
-        let query = "nantucket";
+    fn case_insensitive() {
+        let query = "ruST";
         let contents = "\
 Rust:
 safe, fast, productive.
 Pick three.";
-        assert_eq!(vec!["safe","fast","productive"],search(query,contents));
+        
+        assert_eq!(vec!["Rust:"], search_case_insensitive(query, contents));
+
+    }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use swangrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = swangrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
