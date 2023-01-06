fab00c9fc5db4c63caffa5c081ac18216a27f038
Ryan Swanson <ryan.s.swanson@gmail.com>
2023-01-06 09:28:54 -0500

    test and skeleton of search()

--- src/lib.rs
+++ src/lib.rs
@@ -25,3 +25,21 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn one_result() {
+        let query = "nantucket";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe","fast","productive"],search(query,contents));
+    }
+}
