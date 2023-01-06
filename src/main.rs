c4ff6c8c2d3f92de6d1f2929d68834eb8d69b924
Ryan Swanson <ryan.s.swanson@gmail.com>
2023-01-05 22:13:31 -0500

    break into a lib

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,17 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use swangrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        error(err);
+        println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    if let Err(e) = run(config) {
+    if let Err(e) = swangrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config { 
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough args");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        
-        Ok( Config { query, file_path } )
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-    println!("{contents}");
-    Ok(())
-}
-
-fn error(message: &str) {
-    println!("Problem parsing arguments: {message}");
 }
