bdf6e9c5f8efa8082eae623662ef16ef48945309
Ryan Swanson <ryan.s.swanson@gmail.com>
2023-01-07 22:41:22 -0500

    closures are cleaner

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,9 @@ use std::process;
 
 use swangrep::Config;
 
-use swangrep::Config;
-
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
